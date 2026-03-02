e4764404858584f96d78302e54377bde56df54f1
Sugar920 <2845757609@qq.com>
2026-03-02 21:05:50 +0800

    update

--- test/registered/ascend/llm_models/test_ascend_llama4_scount_17b_16e.py
+++ test/registered/ascend/llm_models/test_ascend_llama4_scount_17b_16e.py
@@ -1,7 +1,9 @@
 import unittest
 
 from sglang.test.ascend.gsm8k_ascend_mixin import GSM8KAscendMixin
-from sglang.test.ascend.test_ascend_utils import LLAMA_4_SCOUT_17B_16E_INSTRUCT_WEIGHTS_PATH
+from sglang.test.ascend.test_ascend_utils import (
+    LLAMA_4_SCOUT_17B_16E_INSTRUCT_WEIGHTS_PATH,
+)
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import CustomTestCase
 
