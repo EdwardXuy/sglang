8a66c9246b09be03aa4a9660bc7ddbe6af08d886
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-03-05 19:23:57 +0800

    test_ascend_logging_npu_full.py debug

--- test/registered/ascend/basic_function/parameter/test_ascend_logging_npu_full.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_logging_npu_full.py
@@ -439,7 +439,7 @@ class BaseTestRequestLogger:
             os.environ[key] = value
 
         cls.process = popen_launch_server(
-            "Qwen/Qwen3-0.6B",
+            MODEL_PATH,
             DEFAULT_URL_FOR_TEST,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
             other_args=other_args,
