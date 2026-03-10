a6b349a1360c8cb5f3cfff06af533b674978e76a
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-03-10 14:49:04 +0800

    test_ascend_logging.py 用例提交

--- test/registered/ascend/basic_function/parameter/test_ascend_logging.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_logging.py
@@ -9,8 +9,7 @@ import requests
 
 from sglang.srt.utils import kill_process_tree
 
-# from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH as MODEL_PATH
-MODEL_PATH = "/home/weights/Llama-3.2-1B-Instruct"
+from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH as MODEL_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import (
     DEFAULT_URL_FOR_TEST,
