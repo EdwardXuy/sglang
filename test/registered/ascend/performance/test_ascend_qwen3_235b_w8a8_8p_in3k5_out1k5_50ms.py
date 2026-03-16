9cfc494f0bc1535a12c8c514d395d6e56dc7c852
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-03-16 14:43:30 +0800

    Update test_ascend_qwen3_235b_w8a8_8p_in3k5_out1k5_50ms.py

--- test/registered/ascend/performance/test_ascend_qwen3_235b_w8a8_8p_in3k5_out1k5_50ms.py
+++ test/registered/ascend/performance/test_ascend_qwen3_235b_w8a8_8p_in3k5_out1k5_50ms.py
@@ -17,7 +17,7 @@ register_npu_ci(
 QWEN3_235B_ENVS = {
     "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
     "SGLANG_DISAGGREGATION_BOOTSTRAP_TIMEOUT": "600",
-    "HCCL_BUFFSIZE": "1600",
+    "HCCL_BUFFSIZE": "570",
     "HCCL_SOCKET_IFNAME": "lo",
     "GLOO_SOCKET_IFNAME": "lo",
     "HCCL_OP_EXPANSION_MODE": "AIV",
@@ -25,6 +25,10 @@ QWEN3_235B_ENVS = {
     "SGLANG_ENABLE_SPEC_V2": "1",
     "SGLANG_SCHEDULER_DECREASE_PREFILL_IDLE": "1",
     "SGLANG_PREFILL_DELAYER_MAX_DELAY_PASSES": "100",
+    "SGLANG_NPU_PROFILING": "0",
+    "SGLANG_NPU_PROFILING_BS": "27",
+    "SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK": "188416",
+    "SGLANG_NPU_FUSED_MOE_MODE": "2",
 }
 
 QWEN3_235B_OTHER_ARGS = [
@@ -40,52 +44,54 @@ QWEN3_235B_OTHER_ARGS = [
     "--quantization",
     "modelslim",
     "--max-running-requests",
-    272,
+    432,
     "--context-length",
     8192,
     "--dtype",
     "bfloat16",
     "--chunked-prefill-size",
-    32768,
+    94208,
     "--max-prefill-tokens",
-    32768,
+    458880,
+    "--sampling-backend",
+    "ascend",
+    "--ep-dispatch-algorithm",
+    "static",
+    "--init-expert-location",
+    "/mnt/share/chenxu/hot_map/235B_3_5k_bs26_decode.pt",
+    "--disable-radix-cache",
+    "--moe-a2a-backend",
+    "ascend_fuseep",
     "--speculative-algorithm",
     "EAGLE3",
     "--speculative-draft-model-path",
     QWEN3_235B_A22B_EAGLE_MODEL_PATH,
     "--speculative-num-steps",
-    3,
+    "3",
     "--speculative-eagle-topk",
-    1,
+    "1",
     "--speculative-num-draft-tokens",
-    4,
-    "--disable-radix-cache",
-    "--moe-a2a-backend",
-    "deepep",
-    "--deepep-mode",
-    "auto",
+    "4",
     "--speculative-draft-model-quantization",
     "unquant",
-    "--tp-size",
-    16,
+    "--tp",
+    "16",
     "--dp-size",
-    16,
+    "16",
     "--enable-dp-attention",
     "--enable-dp-lm-head",
     "--mem-fraction-static",
-    0.8,
+    "0.8",
     "--cuda-graph-bs",
-    3,
-    4,
-    6,
-    8,
-    10,
-    12,
-    13,
-    14,
-    15,
-    16,
-    17,
+    "1",
+    "2",
+    "4",
+    "8",
+    "16",
+    "20",
+    "24",
+    "26",
+    "27",
 ]
 
 
@@ -94,14 +100,14 @@ class TestQwen235B(TestAscendPerformanceTestCaseBase):
     other_args = QWEN3_235B_OTHER_ARGS
     envs = QWEN3_235B_ENVS
     dataset_name = "random"
-    max_concurrency = 272
+    max_concurrency = 432
     num_prompts = int(max_concurrency) * 4
     input_len = 3500
     output_len = 1500
     random_range_ratio = 1
-    tpot = 48.5
+    tpot = 50.1
     # T: 290@50ms.   800I: 1.1*T
-    output_token_throughput = 4087
+    output_token_throughput = 6189
 
     def test_qwen3_235b(self):
         self.run_throughput()
