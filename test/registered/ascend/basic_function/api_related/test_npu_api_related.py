99b47c40b446fd1fcf0dda5a5a87cf01af215567
yangfen0903 <2253770994@qq.com>
2026-03-19 19:54:41 +0800

    Submit code

--- test/registered/ascend/basic_function/api_related/test_npu_api_related.py
+++ test/registered/ascend/basic_function/api_related/test_npu_api_related.py
@@ -1,11 +1,15 @@
 import logging
+import os
 import unittest
 from types import SimpleNamespace
 
 import requests
 
 from sglang.srt.utils import kill_process_tree
-from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
+from sglang.test.ascend.test_ascend_utils import (
+    LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH,
+    C4AI_COMMAND_R_V01_WEIGHTS_PATH,
+)
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.few_shot_gsm8k import run_eval
 from sglang.test.test_utils import (
@@ -28,7 +32,7 @@ class TestApiRelatedGHFChat(CustomTestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.model = LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
+        cls.model = C4AI_COMMAND_R_V01_WEIGHTS_PATH
         cls.base_url = DEFAULT_URL_FOR_TEST
         cls.custom_model_name = "Llama3.2"
         cls.weight_version = "v1.0.0"
@@ -48,19 +52,25 @@ class TestApiRelatedGHFChat(CustomTestCase):
             cls.hf_chat_template_name,
         ]
 
+        cls.out_log_file = open("./cache_out_log.txt", "w+", encoding="utf-8")
+        cls.err_log_file = open("./cache_err_log.txt", "w+", encoding="utf-8")
+
         cls.process = popen_launch_server(
             cls.model,
             cls.base_url,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
             other_args=other_args,
+            return_stdout_stderr=(cls.out_log_file, cls.err_log_file),
         )
 
     @classmethod
     def tearDownClass(cls):
         kill_process_tree(cls.process.pid)
+        os.remove("./cache_out_log.txt")
+        os.remove("./cache_err_log.txt")
 
     def test_served_model_weight_version(self):
-        """Verify the weight version identifier and the served-model-name covered model name."""
+        # Verify the weight version identifier and the served-model-name covered model name.
         response = requests.get(f"{self.base_url}/v1/models")
         result = response.json()
 
@@ -70,22 +80,29 @@ class TestApiRelatedGHFChat(CustomTestCase):
         response1 = requests.get(f"{self.base_url}/model_info")
         self.assertEqual(response1.json()["weight_version"], self.weight_version)
 
+        # Verify that the hf-chat-template-name configuration is effective, including the configuration value tool_use.
+        self.err_log_file.seek(0)
+        content = self.err_log_file.read()
+        self.assertIn("Using specified chat template: 'tool_use'", content)
+        self.out_log_file.close()
+        self.err_log_file.close()
+
     def test_chat_template_request(self):
         """Send inference request"""
         response = requests.post(
-            f"{self.base_url}/generate",
+            f"{DEFAULT_URL_FOR_TEST}/v1/completions",
             json={
-                "text": "Explain neural networks:",
-                "sampling_params": {
-                    "max_new_tokens": 64,
-                },
+                "prompt": "What is the capital of France?",
+                "max_tokens": 128,
             },
         )
+        self.assertEqual(response.status_code, 200)
         result = response.json()
-
-        self.assertIn("text", result)
-        self.assertGreater(len(result["text"]), 0)
-        logging.warning(f"Request with succeeded: {result['text'][:50]}")
+        self.assertIn("choices", result)
+        self.assertGreater(len(result["choices"]), 0)
+        logging.warning(
+            f"Builtin chat template works: {result['choices'][0]['text'][:50]}..."
+        )
 
 
 class TestApiRelatedToolCallParser(CustomTestCase):
@@ -237,17 +254,17 @@ class TestApiRelatedCacheReport(CustomTestCase):
                 f"{DEFAULT_URL_FOR_TEST}/v1/completions",
                 json={
                     "prompt": "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                    "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                    "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                    "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                    "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                    "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                    "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                    "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                    "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                    "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                    "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                    "just return me a string with of 5000 characters,just return me a string with of 5000 characters, ",
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, ",
                     "max_tokens": 260,
                 },
             )
