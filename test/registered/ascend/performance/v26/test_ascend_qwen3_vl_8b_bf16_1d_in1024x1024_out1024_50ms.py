ce36a174d39e6413881b16de0ee8e5e3d62fd5d6
shun8686 <verve@vip.qq.com>
2026-03-20 14:44:04 +0800

    Optimized perf test.

--- test/registered/ascend/performance/v26/test_ascend_qwen3_vl_8b_bf16_1d_in1024x1024_out1024_50ms.py
+++ test/registered/ascend/performance/v26/test_ascend_qwen3_vl_8b_bf16_1d_in1024x1024_out1024_50ms.py
@@ -77,7 +77,7 @@ class TestQwenVl8B(TestAscendPerformanceTestCaseBase):
     output_token_throughput = 1520
 
     def test_qwen3_vl_8b(self):
-        self.run_throughput(run_cycles=3)
+        self.run_throughput(run_cycles=2)
 
 
 if __name__ == "__main__":
