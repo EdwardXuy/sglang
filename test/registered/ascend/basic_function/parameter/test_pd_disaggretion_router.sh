6388b49eb4f31e55f23cc7d0e06e5fba81fca0ce
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-03-23 16:08:39 +0800

    test_ascend_load_balance_method.py debug

--- test/registered/ascend/basic_function/parameter/test_pd_disaggretion_router.sh
+++ test/registered/ascend/basic_function/parameter/test_pd_disaggretion_router.sh
@@ -1,5 +1,5 @@
 python -m sglang_router.launch_router \
-    --pd-disaggregation
+    --pd-disaggregation \
     --prefill http://127.0.0.1:8080 8998 \
     --decode http://127.0.0.1:8081 \
     --host 127.0.0.1 \
