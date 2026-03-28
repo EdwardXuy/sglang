914928f1a2e9cf5f45bf64488e56147ac625a84f
liuxianglong17 <liuxianglong17@mails.ucas.ac.cn>
2026-03-28 17:46:42 +0800

    testcase for quantization and data type feature

--- test/registered/ascend/basic_function/parameter/test_npu_enable_fp32_lm_head.py
+++ test/registered/ascend/basic_function/parameter/test_npu_enable_fp32_lm_head.py
@@ -4,6 +4,7 @@ from sglang.test.ascend.gsm8k_ascend_mixin import GSM8KAscendMixin
 from sglang.test.ascend.test_ascend_utils import QWEN3_32B_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import CustomTestCase
+
 register_npu_ci(
     est_time=300,
     suite="nightly-4-npu-a3",
