fb3e5e220b21ea237b1301db9826b54fdd24cc26
cy <chenyang08056032@163.com>
2026-04-07 22:44:09 +0800

    Changing the images to be loaded from teh local

--- test/registered/ascend/embedding_models/test_npu_gme_qwen2_vl_2b.py
+++ test/registered/ascend/embedding_models/test_npu_gme_qwen2_vl_2b.py
@@ -5,7 +5,7 @@ import torch
 
 from sglang.test.ascend.test_ascend_utils import (
     GME_QWEN2_VL_2B_INSTRUCT_WEIGHTS_PATH,
-    IMAGES_023_PATH
+    IMAGES_023_PATH,
 )
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.runners import HFRunner, SRTRunner
